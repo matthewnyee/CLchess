643bbd147855ca6c3796e7bf111f0678becdf6be
Matt Yee <mattyee@Matts-MacBook-Pro.local>
2015-10-08 16:59:29 -0700

    board displaying (without pieces)

--- board.rb
+++ board.rb
@@ -1,9 +1,12 @@
 class Board
 
+  attr_accessor :highlight
+
   def initialize
     @grid = Array.new(8) { Array.new(8) {nil} }
-    fill_black_pieces
-    fill_white_pieces
+    #fill_black_pieces
+    #fill_white_pieces
+    @highlight = [0, 0]
   end
 
   def move(start_pos, end_pos)
@@ -12,6 +15,18 @@ class Board
     #make start_pos nil
     #raise exception if no piece at start_pos
     #raise exception if cannot move to end_pos
+  end
+
+  def display
+    print "\n"*20
+    (0...8).each do |y|
+      print "#{8-y}  "
+      print "\n"
+    end
+    print "\n"
+    print "   "
+    ('A'..'H').each { |letter| print " #{letter} "}
+    print "\n"*3
   end
 
 end
--- game.rb
+++ game.rb
@@ -5,6 +5,15 @@ class Game
 
   def initialize
     @board = Board.new
+    @board.display
   end
 
+  def get_player_input
+    STDIN.getch
+    modify(@board.highlight)
+  end
+
+
 end
+
+Game.new.play
