899781ea4aee9fe974f8fa08d607ee1271767427
Matt Yee <mattyee@Matts-MacBook-Pro.local>
2015-10-08 17:31:01 -0700

    add highlight square function

--- board.rb
+++ board.rb
@@ -6,7 +6,7 @@ class Board
     @grid = Array.new(8) { Array.new(8) {nil} }
     #fill_black_pieces
     #fill_white_pieces
-    @highlight = [0, 0]
+    @highlight = [5, 0]
   end
 
   def move(start_pos, end_pos)
@@ -19,14 +19,26 @@ class Board
 
   def display
     print "\n"*20
-    (0...8).each do |y|
-      print "#{8-y}  "
+    (0...8).each do |y_coord|
+      print "#{8-y_coord}  "
+      (0...8).each do |x_coord|
+        if @highlight[0] == x_coord && @highlight[1] == (7 - y_coord)
+          print highlight_square(x_coord, y_coord)
+        else
+          print "   "
+        end
+      end
       print "\n"
     end
     print "\n"
     print "   "
     ('A'..'H').each { |letter| print " #{letter} "}
     print "\n"*3
+    highlight_square
+  end
+
+  def highlight_square(x, y)
+    " H "
   end
 
 end
